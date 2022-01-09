0f13c778bc7290285b46eea893d71e9224de3218
Pablo García Peris <pablogperis@gmail.com>
2022-01-09 18:13:59 +0100

    Calibración sensores incluyendo láser

--- 02_Matlab/Calibracion_sensores.m
+++ 02_Matlab/Calibracion_sensores.m
@@ -4,37 +4,55 @@
 clear
 format shortG
 
-global robot_name
+global robot_name laser_name nbalizas
 
 variables_globales
 
 %% Toma de datos
 N_medidas = 500;
-N_sensores = 3;
+N_sensores_us = 3;
 
 apoloPlaceMRobot(robot_name,[4 2 0],-pi/2);
 apoloUpdate();
 
 %% Ultrasonidos (sin moverse)
-medidas = zeros(N_medidas, N_sensores);
+medidas_us = zeros(N_medidas, N_sensores_us);
+medidas_ls = zeros(N_medidas, 2); %solo se miden las distancias y ángulo del sensor nº 1
 
 for i = 1:N_medidas
-    medidas_next = GetUltrasonicSensorsWithNoise(robot_name);
+    medidas_next_us = GetUltrasonicSensorsWithNoise(robot_name);
+    medidas_next_ls = GetLaserData(laser_name, nbalizas);
 
-    medidas(i, :) = medidas_next;
+    medidas_us(i, :) = medidas_next_us;
+    medidas_ls(i, :) = medidas_next_ls(1:2)';
+
 end
+
+var_med_us = var(medidas_us);
+var_med_ls = var(medidas_ls);
+
+R = diag([var_med_us repmat(var_med_ls, 1, nbalizas)])
 
 %% Dibujos
-figure;
-for i = 1:N_sensores
+for i = 1:N_sensores_us
+    figure;
     hold on
-    plot(1:N_medidas, medidas(:, i));
+    plot(1:N_medidas, medidas_us(:, i));
     xlabel('z (m)');
-    ylabel('medida');
+    ylabel('medida')
+    title("ultrasonidos #" + num2str(i));
 end
 
-var_med = var(medidas);
+figure;
+plot(1:N_medidas, medidas_ls(:, 1));
+xlabel('z (rad)');
+ylabel('medida')
+title('ángulo con baliza con láser');
 
-R = diag(var_med)
+figure;
+plot(1:N_medidas, medidas_ls(:, 2));
+xlabel('z (m)');
+ylabel('medida')
+title('distancia a baliza con láser');
 
 save calibracion_sensores R
